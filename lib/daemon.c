6fb620d95157b290e79991fde6f0afa19d2a56e0
popastefan10 <popashtefan10@gmail.com>
2023-01-12 20:16:39 +0200

    create daemon skeleton

---
+++ lib/daemon.c
@@ -0,0 +1,60 @@
+/**
+ * This daemon skeleton is based on an answer to the stackoverflow question:
+ * https://stackoverflow.com/questions/17954432/creating-a-daemon-in-linux/17955149#17955149
+ * Fork this code: https://github.com/pasce/daemon-skeleton-linux-c
+ * Read about it: https://nullraum.net/how-to-create-a-daemon-in-c/
+ */
+
+#include "daemon.h"
+
+void skeleton_daemon() {
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+}
---
+++ lib/daemon.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+void skeleton_daemon();
